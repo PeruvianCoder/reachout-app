63b76d00e616aeb8ce879376205353bd1bd3dfd0
GMDays <cgrij001@fiu.edu>
2020-07-04 00:03:22 -0700

    more restructuring, adding intial feed page

---
+++ src/components/Feed.js
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+import { Text, View } from "react-native";
+
+export default function Load(props) {
+  return <Text>Hello, this is the feed Page!</Text>;
+}
